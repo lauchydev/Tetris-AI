0a9c66b22ded280ceffe1cf885d9c6909b6336de
AidenRed8 <aiden.webster@griffithuni.edu.au>
2024-07-24 12:06:56 +1000

    Created the Main Screen

---
+++ src/MainScreen.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class MainScreen extends Panel {
+
+    public MainScreen(Tetris tetris) {
+        setLayout(new GridLayout(4, 1));
+
+
+    }
+}
---
+++ src/Tetris.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+
+public class Tetris extends Frame {
+    private MainScreen mainScreen;
+
+
+    public Tetris() {
+        initUI();
+    }
+
+    private void initUI() {
+        mainScreen = new MainScreen(this);
+        add(mainScreen);
+        setTitle("Tetris");
+        setSize(800, 500);
+        setResizable(false);
+        setVisible(true);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        new Tetris();
+    }
+}
