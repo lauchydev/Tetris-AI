35377dd1a514ddf42c2227851316c400e420da31
AidenRed8 <aiden.webster@griffithuni.edu.au>
2024-08-07 16:54:06 +1000

    Added Custom Splash Screen Image and Fixed the sizing of the image

--- src/main/SplashScreen.java
+++ src/main/SplashScreen.java
@@ -12,7 +12,7 @@ public class SplashScreen extends JPanel {
     public SplashScreen(Tetris parentFrame) {
         this.parentFrame = parentFrame;
 
-        splashImage = new ImageIcon(getClass().getResource("/path/to/image.jpg")).getImage();
+        splashImage = new ImageIcon(getClass().getResource("/Images/Splash_Screen.jpg")).getImage();
         this.setPreferredSize(new Dimension(Tetris.frameWidth, Tetris.frameHeight));
 
 
@@ -27,6 +27,31 @@ public class SplashScreen extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(splashImage, 0, 0, Tetris.frameWidth, Tetris.frameHeight, this);
+        // Calculate the aspect ratio of the image and the panel
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+        int imageWidth = splashImage.getWidth(this);
+        int imageHeight = splashImage.getHeight(this);
+
+        double panelAspectRatio = (double) panelWidth / panelHeight;
+        double imageAspectRatio = (double) imageWidth / imageHeight;
+
+        int drawWidth, drawHeight;
+        if (imageAspectRatio > panelAspectRatio) {
+            // Scale image to fit width
+            drawWidth = panelWidth;
+            drawHeight = (int) (panelWidth / imageAspectRatio);
+        } else {
+            // Scale image to fit height
+            drawHeight = panelHeight;
+            drawWidth = (int) (panelHeight * imageAspectRatio);
+        }
+
+        // Calculate the x and y positions to center the image
+        int x = (panelWidth - drawWidth) / 2;
+        int y = (panelHeight - drawHeight) / 2;
+
+        // Draw the scaled image
+        g.drawImage(splashImage, x, y, drawWidth, drawHeight, this);
     }
 }
