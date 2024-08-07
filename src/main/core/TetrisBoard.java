ae9ea827264996fa421620aba2a1f1b8b10520de
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-08-07 18:29:51 +1000

    Implement piece locking, line clears, and game over state

--- src/main/core/TetrisBoard.java
+++ src/main/core/TetrisBoard.java
@@ -47,6 +47,10 @@ public class TetrisBoard {
         return this.activePiece;
     }
 
+    public boolean isGameOver() {
+        return this.activePiece == null;
+    }
+
     public boolean rotateClockwise() {
         return this.rotateActivePiece(true);
     }
@@ -65,6 +69,36 @@ public class TetrisBoard {
 
     public boolean softDrop() {
         return this.shiftActivePiece(0, -1);
+    }
+
+    public PlacementResult hardDrop() {
+        if (this.activePiece == null) {
+            return null;
+        }
+
+        // softDrop has a side effect
+        //noinspection StatementWithEmptyBody
+        while (this.softDrop()) {}
+        for (var cell : this.activePiece.getCells()) {
+            if (cell.y() < this.height) {
+                this.rows.get(cell.y()).set(cell.x(), true);
+            }
+        }
+
+        this.rows.removeIf(row -> row.stream().allMatch(cell -> cell));
+        int linesCleared = this.height - this.rows.size();
+        while (this.rows.size() < this.height) {
+            var row = new ArrayList<Boolean>();
+            for (int x = 0; x < width; x++) {
+                row.add(false);
+            }
+            this.rows.add(row);
+        }
+
+        var nextPiece = new ActivePiece(Piece.I, Rotation.North, 4, 19);
+        this.activePiece = !nextPiece.collides(this) ? nextPiece : null;
+
+        return new PlacementResult(linesCleared);
     }
 
     private boolean rotateActivePiece(boolean clockwise) {
