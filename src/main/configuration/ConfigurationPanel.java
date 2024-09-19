db5f41671c059fa62eba97c6d1b86745f6fb546a
Patrick Nazar (windows) <pat@antirealm.com>
2024-09-19 11:54:58 +0930

    configuration screen UI improvements

---
+++ src/main/configuration/ConfigurationPanel.java
@@ -0,0 +1,99 @@
+package main.configuration;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ConfigurationPanel extends JPanel {
+
+    @FunctionalInterface
+    private interface ValueUpdateAction<T> {
+        void update(T value);
+    }
+
+    @FunctionalInterface
+    private interface GetConfigValueAction<T> {
+        T getConfigValue();
+    }
+
+    ConfigurationPanel() {
+        Configuration config = Configuration.getInstance();
+        setLayout(new GridLayout(8, 2));
+        setOpaque(false);
+
+        // Sliders
+        createSlider("Field Width (No. of cells):", 5, 15, config::getFieldWidth, config::setFieldWidth);
+        createSlider("Field Height (No. of cells):", 15, 30, config::getFieldHeight, config::setFieldHeight);
+        createSlider("Game Level:", 1, 10, config::getGameLevel, config::setGameLevel);
+
+        // Check Boxes
+        createCheckbox("Music (On|Off):", config::getMusicOn, config::setMusicOn);
+        createCheckbox("Sound Effect (On|Off):", config::getSoundOn, config::setSoundOn);
+        createCheckbox("AI Play (On|Off):", config::getAIPlayOn, config::setAIPlayOn);
+        createCheckbox("Extend Mode (On|Off):", config::getExtendModeOn, config::setExtendModeOn);
+    }
+
+    private JLabel createTitleLabel(String title) {
+        JLabel titleLabel = new JLabel(title);
+        titleLabel.setForeground(Color.WHITE);
+        return titleLabel;
+    }
+
+    private JLabel createValueLabel() {
+        JLabel titleLabel = new JLabel();
+        titleLabel.setForeground(Color.WHITE);
+        titleLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        return titleLabel;
+    }
+
+    private void updateCheckboxLabel(boolean val, JLabel valueLabel) {
+        valueLabel.setText(val ? "On" : "Off");
+    }
+
+    // Modify the createCheckbox method in the ConfigurationScreen class
+    private void createCheckbox(String title, GetConfigValueAction<Boolean> getFn, ValueUpdateAction<Boolean> updateAction) {
+        JCheckBox checkbox = new JCheckBox();
+        checkbox.setOpaque(false);
+        JLabel titleLabel = createTitleLabel(title);
+        JLabel valueLabel = createValueLabel();
+
+        // Set the initial state of the checkbox based on the configuration
+        boolean isSelected = getFn.getConfigValue();
+        checkbox.setSelected(isSelected);
+        updateCheckboxLabel(isSelected, valueLabel);
+
+        add(titleLabel);
+        add(checkbox);
+        add(valueLabel);
+        checkbox.addItemListener(e -> {
+            boolean selected = checkbox.isSelected();
+            updateAction.update(selected);
+            updateCheckboxLabel(selected, valueLabel);
+        });
+    }
+
+    private void updateSliderLabel(int val, JLabel valueLabel) {
+        valueLabel.setText(Integer.toString(val));
+    }
+
+    private void createSlider(String title, int min, int max, GetConfigValueAction<Integer> getFn, ValueUpdateAction<Integer> updateAction) {
+        var initValue = getFn.getConfigValue();
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, min, max, initValue);
+        slider.setForeground(Color.WHITE);
+        slider.setOpaque(false);
+        JLabel titleLabel = createTitleLabel(title);
+        JLabel valueLabel = createValueLabel();
+        slider.setMajorTickSpacing(1);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        updateSliderLabel(initValue, valueLabel);
+        slider.addChangeListener(e -> {
+            int val = slider.getValue();
+            updateAction.update(val);
+            updateSliderLabel(val, valueLabel);
+        });
+        add(titleLabel);
+        add(slider);
+        add(valueLabel);
+    }
+
+}
--- src/main/configuration/ConfigurationScreen.java
+++ src/main/configuration/ConfigurationScreen.java
@@ -4,89 +4,17 @@ import main.Tetris;
 import main.ui.BasicScreen;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ConfigurationScreen extends BasicScreen {
 
-    @FunctionalInterface
-    private interface ValueUpdateAction<T> {
-        void update(T value);
-    }
-
-    @FunctionalInterface
-    private interface GetConfigValueAction<T> {
-        T getConfigValue();
-    }
-
     public ConfigurationScreen(Tetris parentFrame) {
         super(parentFrame, "Configuration");
-        Configuration config = Configuration.getInstance();
-
-        // Sliders
-        createSlider("Field Width (No. of cells):", 5, 15, 0, config::getFieldWidth, config::setFieldWidth);
-        createSlider("Field Height (No. of cells):", 15, 30, 1, config::getFieldHeight, config::setFieldHeight);
-        createSlider("Game Level:", 1, 10, 2, config::getGameLevel, config::setGameLevel);
-
-        // Check Boxes
-        createCheckbox("Music (On|Off):", 0, config::getMusicOn, config::setMusicOn);
-        createCheckbox("Sound Effect (On|Off):", 1, config::getSoundOn, config::setSoundOn);
-        createCheckbox("AI Play (On|Off):", 2, config::getAIPlayOn, config::setAIPlayOn);
-        createCheckbox("Extend Mode (On|Off):", 3, config::getExtendModeOn, config::setExtendModeOn);
+        ConfigurationPanel  configurationPanel = new ConfigurationPanel();
+        setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100)); // Padding
+        add(configurationPanel, BorderLayout.CENTER);
     }
 
-    private void updateCheckboxLabel(boolean val, JLabel valueLabel) {
-        valueLabel.setText(val ? "On" : "Off");
-    }
 
-    // Modify the createCheckbox method in the ConfigurationScreen class
-    private void createCheckbox(String title, int pos, GetConfigValueAction<Boolean> getFn, ValueUpdateAction<Boolean> updateAction) {
-        JCheckBox checkbox = new JCheckBox();
-        int y = 100 + 60 * (3 + pos);
-        JLabel titleLabel = new JLabel(title);
-        JLabel valueLabel = new JLabel();
-
-        // Set the initial state of the checkbox based on the configuration
-        boolean isSelected = getFn.getConfigValue();
-        checkbox.setSelected(isSelected);
-        updateCheckboxLabel(isSelected, valueLabel);
-
-        checkbox.setBounds(250, y, 200, 50);
-        valueLabel.setBounds(550, y, 20, 50);
-        titleLabel.setBounds(50, y, 200, 50);
-        add(checkbox);
-        add(valueLabel);
-        add(titleLabel);
-        checkbox.addItemListener(e -> {
-            boolean selected = checkbox.isSelected();
-            updateAction.update(selected);
-            updateCheckboxLabel(selected, valueLabel);
-        });
-    }
-
-    private void updateSliderLabel(int val, JLabel valueLabel) {
-        valueLabel.setText(Integer.toString(val));
-    }
-
-    private void createSlider(String title, int min, int max, int pos, GetConfigValueAction<Integer> getFn, ValueUpdateAction<Integer> updateAction) {
-        var initValue = getFn.getConfigValue();
-        JSlider slider = new JSlider(JSlider.HORIZONTAL, min, max, initValue);
-        JLabel titleLabel = new JLabel(title);
-        JLabel valueLabel = new JLabel();
-        int y = 100 + 60 * pos;
-        slider.setBounds(250, y, 300, 50);
-        valueLabel.setBounds(550, y, 20, 50);
-        titleLabel.setBounds(50, y, 200, 50);
-        slider.setMajorTickSpacing(1);
-        slider.setPaintTicks(true);
-        slider.setPaintLabels(true);
-        updateSliderLabel(initValue, valueLabel);
-        slider.addChangeListener(e -> {
-            int val = slider.getValue();
-            updateAction.update(val);
-            updateSliderLabel(val, valueLabel);
-        });
-        add(titleLabel);
-        add(valueLabel);
-        add(slider);
-    }
 
 }
